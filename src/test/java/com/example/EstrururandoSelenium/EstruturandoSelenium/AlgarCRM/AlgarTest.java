aa7c2b7db528c77aed5340ea8f883856658d8a27
higorlaf12 <higor.freitas@neppoadm.com.br>
2017-12-19 10:25:57 -0200

    Alteraçãoes

--- src/test/java/com/example/EstrururandoSelenium/EstruturandoSelenium/AlgarCRM/AlgarTest.java
+++ src/test/java/com/example/EstrururandoSelenium/EstruturandoSelenium/AlgarCRM/AlgarTest.java
@@ -32,6 +32,6 @@ public class AlgarTest {
 
     private void userPageLoginAlgar(){
         pageLoginAlgar = new PageLoginAlgar(PageAlgar.getDriver());
-        pageLoginAlgar.loginPage("consult_vendas_1","ctbc2014@");
+        pageLoginAlgar.loginPage("","");
     }
 }
